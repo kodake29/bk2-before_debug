1e891310d8bd574b93d815217fd4d26f9c8a5bc3
vagrant <vagrant@localhost.localdomain>
2020-09-02 07:55:48 +0000

    [finish]favorite

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,14 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :books, dependent: :destroy
+  has_many :favorites, dependent: :destroy
   attachment :profile_image, destroy: false
 
   #バリデーションは該当するモデルに設定する。エラーにする条件を設定できる。
   validates :name, length: { in: 2..20 }
   validates :introduction, length: {maximum: 50}
+
+  def already_favorited?(book)
+  	self.favorites.exists?(book_id: book.id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'home#top'
-  resources :books
+  resources :books do
+  	resource :favorites, only: [:create, :destroy]
+  end
   resources :users
   get 'home/about' => 'home#about'
 end
