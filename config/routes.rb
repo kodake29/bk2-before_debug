d20d325fa6f1927f9f106edeb6d62cf0a9c6ffe6
vagrant <vagrant@localhost.localdomain>
2020-09-04 09:04:31 +0000

    [finish] comment

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'home#top'
   resources :books do
+  	resources :book_comments, only: [:create, :destroy]
   	resource :favorites, only: [:create, :destroy]
   end
   resources :users
